7fb1a6da4fa05ec031082ea916458d4bef2e1cf8
Hans Loquet <hans@mycr4ft.be>
2018-01-30 14:47:45 +0100

    scenario

---
+++ Acme.Tests/Scenario.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Acme.Commands;
+using Acme.Events;
+
+namespace Acme.Tests
+{
+    public class Scenario
+    {
+        public Scenario Given(Event anEvent)
+        {
+            return this;
+        }
+
+        public Scenario When(Command aCommand)
+        {
+            return this;
+        }
+
+        public Scenario Then(Event anEvent)
+        {
+            return this;
+        }
+
+        public void Assert()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Acme.Tests/StartShoppingTests.cs
@@ -0,0 +1,28 @@
+﻿using Acme.Commands;
+using Acme.Events;
+using Xunit;
+
+namespace Acme.Tests
+{
+    public class StartShoppingTests {
+
+        [Fact]
+        public void CustomerStartedShopping()
+        {
+            var cartId = "";
+            var customerId = "";
+            var startTime = "";
+
+            new Scenario()
+                .When(new StartShopping (cartId,customerId,startTime))
+                .Then(new CustomerStartedShopping(cartId, customerId))
+                .Assert();
+        }
+
+        public void CustomerStartedShoppingTwice()
+        {
+   
+        }
+
+    }
+}
