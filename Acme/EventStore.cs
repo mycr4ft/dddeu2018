2dcd7f2db494cacc7f84d363acfb699570bf598e
Thomas Coopman <thomas.coopman@gmail.com>
2018-01-31 11:05:25 +0100

    new in memory store

--- Acme/EventStore.cs
+++ Acme/EventStore.cs
@@ -1,12 +1,46 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Acme.Events;
 
 namespace Acme
 {
     public interface EventStore
     {
-        void Add(string streamId, IEnumerable<Event> events);
+        void Add(string streamId, IReadOnlyList<Event> events);
 
-        IEnumerable<Event> ReadStream(string streamId);
+        IReadOnlyList<Event> ReadStream(string streamId);
+    }
+
+    public class InMemoryEventStore : EventStore
+    {
+        private Dictionary<string, IList<Event>> _store;
+
+        public InMemoryEventStore()
+        {
+            _store = new Dictionary<string, IList<Event>>();
+        } 
+        
+        public void Add(string streamId, IReadOnlyList<Event> events)
+        {
+            
+            if (_store.ContainsKey(streamId))
+            {
+                _store[streamId].Concat(events.ToList());
+            }
+            else
+            {
+                _store.Add(streamId, events.ToList());
+            }
+        }
+
+        public IReadOnlyList<Event> ReadStream(string streamId)
+        {
+            if (_store.ContainsKey(streamId))
+            {
+                return _store[streamId].ToList();
+            }
+            throw new ArgumentException("Stream not found");
+        }
     }
 }
