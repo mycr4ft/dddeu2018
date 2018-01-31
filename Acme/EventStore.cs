2408cdc40c28366ad2f94147075a131175b1c3a1
Thomas Coopman <thomas.coopman@gmail.com>
2018-01-31 10:59:08 +0100

    event store

---
+++ Acme/EventStore.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Acme.Events;
+
+namespace Acme
+{
+    public interface EventStore
+    {
+        void Add(string streamId, IEnumerable<Event> events);
+
+        IEnumerable<Event> ReadStream(string streamId);
+    }
+}
