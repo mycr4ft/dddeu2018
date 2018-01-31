5d889d0454bde8650b5ea8fa40e03071aab201c9
Thomas Coopman <thomas.coopman@gmail.com>
2018-01-31 10:49:55 +0100

    steps reporter wip

---
+++ Acme.Tests/StepsReporter.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Acme.Tests
+{
+    public class ScenarioSteps
+    {
+        public readonly IEnumerable<Step> Steps;
+        public readonly string Name;
+
+        public ScenarioSteps(IEnumerable<Step> steps, string name)
+        {
+            Steps = steps;
+            Name = name;
+        }
+    }
+    
+    public class StepsReporter
+    {
+        private readonly IList<ScenarioSteps> _scenarioStepses = new List<ScenarioSteps>();
+
+        private static StepsReporter _singleton;
+        
+        private StepsReporter() {}
+
+        public static StepsReporter Instance()
+        {
+            if (_singleton == null)
+            {
+                _singleton = new StepsReporter();
+            }
+
+            return _singleton;
+        }
+
+        public void Add(ScenarioSteps ss)
+        {
+            _scenarioStepses.Add(ss);
+        }
+
+        public void Build()
+        {
+            foreach (var scenarioStepse in _scenarioStepses)
+            {
+                GenerateHtml.Generate(scenarioStepse.Name, scenarioStepse.Steps);
+            }
+        }
+    }
+}
